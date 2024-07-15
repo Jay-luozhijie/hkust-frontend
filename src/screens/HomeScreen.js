8026ea221518c35f852a39f180046b7b0b164e58
TonyDerek-dot <yu1429422734@gmail.com>
2024-07-16 00:30:37 +0800

    Another-branch

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,142 @@
 import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCoverflow, Pagination, Autoplay } from 'swiper/modules';
 import homeScreenImage from '../img/hkust_background.png';
-import homePageBackground from "../img/homepage-background.png"
+import backgroundImage from '../img/Group12.png';
 import '../css/homeScreen.css';
-import backgroundImage from '../img/Group12.png'; // 确保路径正确
+import { useTranslation } from 'react-i18next';
+import { useMediaQuery } from 'react-responsive';
+import breakpoints from '../config/breakpoints';
+import i18n from 'i18next';
+
+const slideContent = (t, isMobile) => {
+  const fontSizeMain = isMobile ? '24px' : '64px';
+  const fontSizeSub = isMobile ? '18px' : '40px';
+  const isEnglish = i18n.language === 'en';
+
+  // 调整每个文本框的位置
+  const textPosition1 = isMobile ? { right: '20px', top: isEnglish ? '80px' : '80px' } : { left: '142px', top: '70px' };
+  const textPosition2 = isMobile ? { right: '20px', top: isEnglish ? '140px' : '120px' } : { left: '142px', top: '140px' };
+  const textPosition3 = isMobile ? { right: '20px', top: isEnglish ? '210px' : '160px' } : { left: '142px', top: '215px' };
+  const textAlign = isMobile ? 'right' : 'left';
+  
+  return (
+    <div>
+      <img className="layer1" src={homeScreenImage} alt={t('hkust_background')} />
+      <div className="layer2 swiper-cover"></div>
+      <div className="layer3">
+        <div style={{
+          position: 'absolute',
+          color: 'white',
+          fontSize: fontSizeMain,
+          fontFamily: 'OPPO Sans',
+          fontWeight: '400',
+          wordWrap: 'break-word',
+          lineHeight: '1.3',
+          textAlign: textAlign,
+          ...textPosition1
+        }}>
+          {t('slide_text_line1')}
+        </div>
+        <div style={{
+          position: 'absolute',
+          color: 'white',
+          fontSize: fontSizeMain,
+          fontFamily: 'OPPO Sans',
+          fontWeight: '400',
+          wordWrap: 'break-word',
+          lineHeight: '1.3',
+          textAlign: textAlign,
+          ...textPosition2
+        }}>
+          {t('slide_text_line2')}
+        </div>
+        <div style={{
+          position: 'absolute',
+          color: 'white',
+          fontSize: fontSizeSub,
+          fontFamily: 'OPPO Sans',
+          fontWeight: '400',
+          wordWrap: 'break-word',
+          lineHeight: '1.3',
+          textAlign: textAlign,
+          ...textPosition3
+        }}>
+          {t('hkust_quant_trading_society')}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 function HomeScreen() {
+  const { t } = useTranslation();
+  const isMobile = useMediaQuery({ query: breakpoints.mobile });
+  const isDesktop = useMediaQuery({ query: breakpoints.desktop });
+  const isEnglish = i18n.language === 'en';
+  const containerStyle1 = {
+    position: 'relative',
+    paddingTop: isMobile ? '50px' : '70px', // 响应式 paddingTop
+    paddingBottom: isMobile ? '400px' : '20px',
+    marginLeft: isMobile ? '10px' : '100px', // 响应式 marginLeft
+    minHeight: '600px',
+    overflow: 'hidden',
+  };
+  
+  const containerStyle2 = {
+    marginTop: isMobile ? '30px' : '70px', // 手机模式下的marginTop调整
+    paddingBottom: '50px'
+  };
+
+  const textStyle1 = isMobile ? { fontSize: '20px', display: 'inline', lineHeight: '1', margin: '10px 0' } : { lineHeight: '1.5', margin: '10px 0' };
+  const textStyle2 = {
+    fontSize: isMobile ? '20px' : '23px',
+    color: '#9A1E23',
+    display: 'inline',
+    lineHeight: isMobile ? '1.2' : '2',
+    margin: isMobile ? '10px 0' : '20px 0'
+  };
+  const textStyle3 = isMobile ? { fontSize: '20px', display: 'inline', lineHeight: '1', margin: '10px 0' } : { lineHeight: '1.5', margin: '10px 0' };
+  const textStyle4 = isMobile ? { fontSize: '20px', display: 'inline', lineHeight: '1', margin: '10px 0' } : { lineHeight: '1.5', margin: '10px 0' };
+  
+  const titleStyle1 = {
+    color: '#9A1E23',
+    fontSize: isMobile ? '32px' : '64px', // 手机模式下的字号调整
+    marginBottom: '10px'
+  };
+  
+  const titleStyle2 = {
+    color: '#003974',
+    fontSize: isMobile ? '20px' : '40px', // 手机模式下的字号调整
+    marginBottom: '20px'
+  };
+  
+  const imageStyle = isMobile 
+  ? {
+      position: 'absolute',
+      left: '10px',  // 手机模式下的左右位置调整
+      top: isEnglish ? '850px' : '580px',  // 手机模式下的上下位置调整
+      width: '500px', // 手机模式下的宽度调整
+      minHeight: '400px', // 手机模式下的最小高度调整
+      background: `url(${backgroundImage}) no-repeat center center`,
+      backgroundSize: 'cover',
+      zIndex: -1
+    }
+  : {
+      position: 'absolute',
+      right: 0,
+      top: 30,
+      width: '700px',
+      minHeight: '600px',
+      background: `url(${backgroundImage}) no-repeat center center`,
+      backgroundSize: 'cover',
+      zIndex: -1
+    };
+
   return (
-      <div>
-        <Swiper  style={{height:'782px'}}
+    <div>
+      <Swiper
+        style={{ height: '782px' }}
         className="homeSwiper"
         effect={'coverflow'}
         grabCursor={false}
@@ -24,215 +150,51 @@ function HomeScreen() {
           slideShadows: true,
         }}
         autoplay={{
-          delay: 30000, // 自动播放延迟时间（毫秒）
-          disableOnInteraction: false, // 用户交互后不禁用自动播放
+          delay: 30000,
+          disableOnInteraction: false,
         }}
-        loop={true} // 启用循环播放
+        loop={true}
         pagination={{
           clickable: true,
         }}
-        modules={[Pagination, Autoplay]}>
-          <SwiperSlide >
-            <div>
-              <img class="layer1" src={homeScreenImage} alt="HKUST Background" />
-              <div class="layer2 swiper-cover"></div>
-              <div class="layer3">
-                <div style={{
-                  left: '142px',
-                  top: '70px',
-                  position: 'absolute',
-                  color: 'white',
-                  fontSize: '64px', // 默认大部分文本的字体大小
-                  fontFamily: 'OPPO Sans',
-                  fontWeight: '400',
-                  wordWrap: 'break-word',
-                  lineHeight: '1.3',  // 调整行间距
-                  textAlign: 'left'  // 设置文本左对齐
-                }}>
-                  通过知识传递、人脉拓展和竞赛实践，<br/>
-                  赋能未来量化金融领袖<br/>
-                <span style={{ 
-                  position: 'absolute',
-                  top: '170px', // 根据需要调整此值以向上移动文本
-                  fontSize: '40px',
-                  width: '100%' // 确保宽度足够包含文本
-                }}>
-                  HKUST Quant Trading Society
-                </span>
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div>
-              <img class="layer1" src={homeScreenImage} alt="HKUST Background" />
-              <div class="layer2 swiper-cover"></div>
-              <div class="layer3">
-                <div style={{
-                  left: '142px',
-                  top: '70px',
-                  position: 'absolute',
-                  color: 'white',
-                  fontSize: '64px', // 默认大部分文本的字体大小
-                  fontFamily: 'OPPO Sans',
-                  fontWeight: '400',
-                  wordWrap: 'break-word',
-                  lineHeight: '1.3',  // 调整行间距
-                  textAlign: 'left'  // 设置文本左对齐
-                }}>
-                  通过知识传递、人脉拓展和竞赛实践，<br/>
-                  赋能未来量化金融领袖<br/>
-                  <span style={{ 
-                    position: 'absolute',
-                    top: '170px', // 根据需要调整此值以向上移动文本
-                    fontSize: '40px',
-                    width: '100%' // 确保宽度足够包含文本
-                  }}>
-                    HKUST Quant Trading Society
-                  </span>
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div>
-              <img class="layer1" src={homeScreenImage} alt="HKUST Background" />
-              <div class="layer2 swiper-cover"></div>
-              <div class="layer3">
-                <div style={{
-                  left: '142px',
-                  top: '70px',
-                  position: 'absolute',
-                  color: 'white',
-                  fontSize: '64px', // 默认大部分文本的字体大小
-                  fontFamily: 'OPPO Sans',
-                  fontWeight: '400',
-                  wordWrap: 'break-word',
-                  lineHeight: '1.3',  // 调整行间距
-                  textAlign: 'left'  // 设置文本左对齐
-                }}>
-                  通过知识传递、人脉拓展和竞赛实践，<br/>
-                  赋能未来量化金融领袖<br/>
-                  <span style={{ 
-                    position: 'absolute',
-                    top: '170px', // 根据需要调整此值以向上移动文本
-                    fontSize: '40px',
-                    width: '100%' // 确保宽度足够包含文本
-                  }}>
-                    HKUST Quant Trading Society
-                  </span>
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div>
-              <img class="layer1" src={homeScreenImage} alt="HKUST Background" />
-              <div class="layer2 swiper-cover"></div>
-              <div class="layer3">
-                <div style={{
-                  left: '142px',
-                  top: '70px',
-                  position: 'absolute',
-                  color: 'white',
-                  fontSize: '64px', // 默认大部分文本的字体大小
-                  fontFamily: 'OPPO Sans',
-                  fontWeight: '400',
-                  wordWrap: 'break-word',
-                  lineHeight: '1.3',  // 调整行间距
-                  textAlign: 'left'  // 设置文本左对齐
-                }}>
-                  通过知识传递、人脉拓展和竞赛实践，<br/>
-                  赋能未来量化金融领袖<br/>
-                  <span style={{ 
-                    position: 'absolute',
-                    top: '170px', // 根据需要调整此值以向上移动文本
-                    fontSize: '40px',
-                    width: '100%' // 确保宽度足够包含文本
-                  }}>
-                    HKUST Quant Trading Society
-                  </span>
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div>
-              <img class="layer1" src={homeScreenImage} alt="HKUST Background" />
-              <div class="layer2 swiper-cover"></div>
-              <div class="layer3">
-                <div style={{
-                  left: '142px',
-                  top: '70px',
-                  position: 'absolute',
-                  color: 'white',
-                  fontSize: '64px', // 默认大部分文本的字体大小
-                  fontFamily: 'OPPO Sans',
-                  fontWeight: '400',
-                  wordWrap: 'break-word',
-                  lineHeight: '1.3',  // 调整行间距
-                  textAlign: 'left'  // 设置文本左对齐
-                }}>
-                  通过知识传递、人脉拓展和竞赛实践，<br/>
-                  赋能未来量化金融领袖<br/>
-                  <span style={{ 
-                    position: 'absolute',
-                    top: '170px', // 根据需要调整此值以向上移动文本
-                    fontSize: '40px',
-                    width: '100%' // 确保宽度足够包含文本
-                  }}>
-                    HKUST Quant Trading Society
-                  </span>
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
-        </Swiper>
+        modules={[EffectCoverflow, Pagination, Autoplay]}
+      >
+        <SwiperSlide>{slideContent(t, isMobile)}</SwiperSlide>
+        <SwiperSlide>{slideContent(t, isMobile)}</SwiperSlide>
+        <SwiperSlide>{slideContent(t, isMobile)}</SwiperSlide>
+        <SwiperSlide>{slideContent(t, isMobile)}</SwiperSlide>
+        <SwiperSlide>{slideContent(t, isMobile)}</SwiperSlide>
+      </Swiper>
+      <div style={containerStyle1}>
         <div style={{
-  position: 'relative', // 设置为相对定位，作为图片的定位基准
-  paddingTop: '70px', // 增加上边距
-  paddingBottom: '20px', // 增加底部边距
-  marginLeft: '100px',
-  minHeight: '600px', // 设置一个最小高度
-}}>
-  <div style={{
-    maxWidth: '890px',
-    width: 'auto',
-    flexGrow: 0,
-    padding: '20px', // 文本内边距
-    color: '#252525',
-    fontSize: '22px',
-    fontFamily: 'OPPO Sans',
-    fontWeight: '400',
-    lineHeight: '1.4', // 行高
-    textAlign: 'left' // 文本左对齐
-  }}>
-    <div style={{ color: '#9A1E23', fontSize: '64px', marginBottom: '10px' }}>
-      香港科技大学量化交易协会
-    </div>
-    <div style={{ color: '#003974', fontSize: '40px', marginBottom: '20px' }}>
-      HKUST Quant Trading Society
-    </div>
-    <div style={{ marginTop: '70px', paddingBottom: '50px' }}>
-      <span>我们是</span>
-      <span style={{ color: '#9A1E23' }}>香港科技大学量化交易协会（港科宽图）</span>
-      <span>，成立于2023年9月。作为香港科技大学金融数学硕士课程（MAFM）的学生社团，我们致力于帮助学生进入量化行业并建立有效的人脉。我们的校友网络涵盖超过10,000名香港科技大学毕业生。此外，我们计划举办面向全校学生的交易比赛，发掘人才并孵化未来的量化基金。</span><br/><br/>
-      <span>香港科技大学数学系自2006年成立以来，每年招收约800名本科生、硕士生和博士生。作为系里支持的学生领袖平台，我们的校友网络覆盖超过10,000名学生和校友。</span>
-    </div>
-  </div>
-  <div style={{
-    position: 'absolute', // 设置绝对定位
-    right: 0, // 右对齐
-    top: 30, // 从顶部开始
-    width: '700px', 
-    minHeight: '600px', // 设置图像区域的最小高度
-    background: `url(${backgroundImage}) no-repeat center center`, // 使用导入的图片作为背景
-    backgroundSize: 'cover', // 覆盖整个容器
-    zIndex: -1
-  }}></div>
-</div>
-
+          maxWidth: '890px',
+          width: 'auto',
+          flexGrow: 0,
+          padding: '20px',
+          color: '#252525',
+          fontSize: '22px',
+          fontFamily: 'OPPO Sans',
+          fontWeight: '400',
+          lineHeight: '1.4',
+          textAlign: 'left'
+        }}>
+          <div style={titleStyle1}>
+            {t('hkust_quant_trading_association')}
+          </div>
+          <div style={titleStyle2}>
+            {t('hkust_quant_trading_society')}
+          </div>
+          <div style={containerStyle2}>
+            <span style={textStyle1}>{t('we_are')}</span>
+            <span style={textStyle2}>{t('hkust_quant_trading_association2')}</span>
+            <span style={textStyle3}>{t('association_description_1')}</span><br /><br />
+            <span style={textStyle4}>{t('association_description_2')}</span>
+          </div>
         </div>
+        <div style={imageStyle}></div>
+      </div>
+    </div>
   );
 }
+
 export default HomeScreen;
