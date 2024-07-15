63a40244e09a60da06f563571c54d6f511036773
Jay <jayluo36@gmail.com>
2024-07-15 21:04:42 +0800

    change proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: '47.243.123.248:5000',
       changeOrigin: true,
     })
   );
